f65e5f2db14c53f9a18658d8118982b29288de23
Henry Luan <henryluan95@gmail.com>
2023-01-03 20:03:23 -0500

    random password generator

---
+++ Day5/pypassword.py
@@ -0,0 +1,21 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+total_characters = nr_numbers + nr_symbols + nr_letters
+random_characters = [letters, numbers, symbols]
+password = ""
+
+for char in range(0, total_characters):
+    random_type = random_characters[random.randint(0,2)]
+    random_character = random_type[random.randint(0,len(random_type) - 1)]
+    password += random_character
+
+print(password)
