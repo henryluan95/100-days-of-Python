d5262f0441eb1e767bf58c41b3350e7106f369a3
Henry Luan <henryluan95@gmail.com>
2023-01-05 20:26:12 -0500

    Functions in Python

---
+++ Day8/Main.py
@@ -0,0 +1,18 @@
+# def greet():
+#     print("Hello")
+#     print(f'How do you do?')
+#     print("Isn't the weather nice today?")
+#
+# greet()
+
+# def greet_with_name(name):
+#     print(f'Hello {name}')
+#     print(f'How do you do {name}')
+#
+# greet_with_name("Herry")
+
+def greet_with(name, location):
+    print(f'Hello {name}')
+    print(f'Isn\'t the weather in {location} nice today')
+
+greet_with("Henry", "Toronto")
