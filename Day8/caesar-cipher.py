0c7b5a358ee99f90984a920adfc744c7939f7e7f
Henry Luan <henryluan95@gmail.com>
2023-01-07 15:56:58 -0500

    Finished caesar-cipher function

--- Day8/caesar-cipher.py
+++ Day8/caesar-cipher.py
@@ -1,43 +1,62 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+from art import logo
+import numbers
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+print(logo)
+should_continue = True
+while should_continue == True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-    #e.g.
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+    #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+        #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+        #e.g.
+        #plain_text = "hello"
+        #shift = 5
+        #cipher_text = "mjqqt"
+        #print output: "The encoded text is mjqqt"
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+        ##HINT: How do you get the index of an item in a list:
+        #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+        ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-def caesar (text, shift, direction):
-    if(direction == "encode"):
-        alphabet_first_haft = alphabet[0: shift]
-        alphabet_second_haft = alphabet[shift:]
-    else:
-        alphabet_second_haft = alphabet[-shift:]
-        alphabet_first_haft = alphabet[0: -shift]
+    #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
 
-    shifted_alphabet = alphabet_second_haft + alphabet_first_haft
+    def caesar (text, shift, direction):
+        if(shift > 26):
+            shift = shift % 26
 
-    index_list = []
-    for letter in text:
-        index_list.append(alphabet.index(letter))
+        if(direction == "encode"):
+            alphabet_first_haft = alphabet[0: shift]
+            alphabet_second_haft = alphabet[shift:]
+        else:
+            alphabet_second_haft = alphabet[-shift:]
+            alphabet_first_haft = alphabet[0: -shift]
 
-    encoded_string = ""
-    for index in index_list:
-        encoded_string += shifted_alphabet[index]
+        shifted_alphabet = alphabet_second_haft + alphabet_first_haft
 
-    print(encoded_string)
+        index_list = []
+        for letter in text:
+            if (alphabet.count(letter) > 0):
+                index_list.append(alphabet.index(letter))
+            else:
+                index_list.append(letter)
 
-caesar(text,shift, direction)
+        encoded_string = ""
+        for index in index_list:
+            if(isinstance(index, numbers.Number)):
+                encoded_string += shifted_alphabet[index]
+            else:
+                encoded_string += index
+
+        print(f'Your code is {encoded_string}')
+
+    caesar(text, shift, direction)
+    code_again = input("Do you want to continue? Answer yes or no\n")
+    if (code_again == "no"):
+        should_continue = False
+        print("Bye")
