d7689899881e5ab438cb89e2f1a74f655db23cf1
Henry Luan <henryluan95@gmail.com>
2023-01-08 10:47:20 -0500

    grading program

---
+++ Day9/grading-program.py
@@ -0,0 +1,25 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for name in student_scores:
+    if(student_scores[name] <= 70):
+        student_grades[name] = "Fail"
+    elif(student_scores[name] > 70 and student_scores[name] <= 80):
+        student_grades[name] = "Acceptable"
+    elif(student_scores[name] > 80 and student_scores[name] <= 90):
+        student_grades[name] = "Exceeds Expectations"
+    else:
+        student_grades[name] = "Outstanding"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
