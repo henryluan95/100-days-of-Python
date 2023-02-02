dc0bee317ee5da393cf0a557a6e10c2cbf19aeaa
Henry Luan <henryluan95@gmail.com>
2023-02-02 11:00:55 -0500

    turtle

---
+++ Day16/main.py
@@ -0,0 +1,13 @@
+import another_module
+print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("DarkOrange")
+timmy.left(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
