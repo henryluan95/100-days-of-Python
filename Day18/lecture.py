074950f9b69ffb8982b9fa5e1304d88bd824618f
Henry Luan <henryluan95@gmail.com>
2023-02-09 18:59:28 -0500

    Turtle draws circles in circle

--- Day18/lecture.py
+++ Day18/lecture.py
@@ -1,14 +1,24 @@
+import turtle
 from turtle import Turtle, Screen
-
+import random
 tim = Turtle()
 
-num_sides = 4
+turtle.colormode(255)
+turtle.speed("fastest")
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
-for shape_side_n in range(3,11):
-    draw_shape(shape_side_n)
+########### Challenge 5 - Spirograph ########
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
