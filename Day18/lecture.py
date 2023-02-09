19e8ba761bd0e768f00a52b232109d7d287e6ec3
Henry Luan <henryluan95@gmail.com>
2023-02-09 18:35:36 -0500

    Turtle draws shapes

--- Day18/lecture.py
+++ Day18/lecture.py
@@ -2,8 +2,13 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+num_sides = 4
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3,11):
+    draw_shape(shape_side_n)
